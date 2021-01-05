9576129c4621d17a9f6a4f4cf959436df94265d1
harinitj <73476433+harinitj@users.noreply.github.com>
2021-01-05 10:00:49 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,20 @@
+class Ball {
+    constructor(x, y, radius, angle) {
+      var options = { 
+        density: 1, 
+        frictionAir: 0.005};
+  
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius=radius;
+      World.add(world, this.body);
+    }
+  
+    display() {
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      ellipse(0, 0, this.radius);
+      pop();
+    }
+  }
